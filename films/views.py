b23c8b7926396fce188b8876cb226932284517ce
Geronimo/Denso <ferrario.dev@gmail.com>
2023-10-24 10:31:22 -0300

    feat(django setup): films app[models, views, urls], project[urls]

---
+++ films/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.views import View
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Film
+
+class FilmBaseView(View):
+    model = Film
+    fields = '__all__'
+    success_url = reverse_lazy('films:all')
+
+class FilmListView(FilmBaseView, ListView):
+    model = Film
+    template_name = 'film_list.html'
+    context_object_name = 'film_list'
+    def get_queryset(self):
+        return self.model.objects.all()
+
+class FilmDetailView(FilmBaseView, DetailView):
+    model = Film
+    template_name = 'film_detail.html'
+    context_object_name = 'film'
+    def get_queryset(self):
+        query = super(FilmDetailView, self).get_queryset()
+        return query.filter(published=True)
+    
